6b3b42d21d1cc946ba14b0eeb2ac91886e4576a7
jc1garcia <colombia@xkpdestinations.com>
2024-08-05 19:14:42 -0500

    Upload

---
+++ data.js
@@ -0,0 +1,118 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-206-bao",
+      "name": "206 Baño",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.069488926236229,
+          "pitch": 1.1214047429048453,
+          "rotation": 4.71238898038469,
+          "target": "1-206-ac-dos-dobles"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-206-ac-dos-dobles",
+      "name": "206 AC Dos Dobles",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.1775682804202745,
+        "pitch": 0.213273896566335,
+        "fov": 1.2861062247484547
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6910846541968922,
+          "pitch": 0.5443025745460837,
+          "rotation": 0.7853981633974483,
+          "target": "0-206-bao"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-h3-206-",
+      "name": "H3 206 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.1054971738352446,
+        "pitch": -0.18269199478904774,
+        "fov": 1.2861062247484547
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "H3 AC Dos Camas Dobles 206",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
